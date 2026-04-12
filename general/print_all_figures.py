896a9d8a10cc79b30d18fc9cb60a8e7adbfa9100
abharadwaj1 <abharadwaj1@tudelft.nl>
2026-04-12 22:48:48 +0200

    Sync

--- general/print_all_figures.py
+++ general/print_all_figures.py
@@ -43,21 +43,20 @@ def launch_figure_printing(args):
     print_start_banner(start_time)
     import os 
     local_faraday_folder = os.environ['LOCAL_FARADAY_PATH']
-    local_faraday_folder = os.path.join(os.environ['LOCAL_FARADAY_PATH'],"faraday_discussions")
     from subprocess import run
     ## Get all paths
     
-    figure_1ab_path = os.path.join(local_faraday_folder,"figure_scripts","figure_1","Figure_1ab_plot_radial_profiles.py")
-    figure_2f_path = os.path.join(local_faraday_folder,"figure_scripts","figure_2","Figure_2f_atomic_and_guinier_bfactors.py")
-    figure_2g_path = os.path.join(local_faraday_folder,"figure_scripts","figure_2","Figure_2g_plot_local_profile.py")
+    figure_1ab_path = os.path.join(local_faraday_folder,"figure_1","Figure_1ab_plot_radial_profiles.py")
+    figure_2f_path = os.path.join(local_faraday_folder,"figure_2","Figure_2f_atomic_and_guinier_bfactors.py")
+    figure_2g_path = os.path.join(local_faraday_folder,"figure_2","Figure_2g_plot_local_profile.py")
     
-    figure_3c_path = os.path.join(local_faraday_folder,"figure_scripts","figure_3","Figure_3c_plot_radial_profile_perturbed_modemaps.py")
-    figure_4b_path = os.path.join(local_faraday_folder,"figure_scripts","figure_4","Figure_4b_bfactor_distribution_scattered.py")
-    figure_5a_path = os.path.join(local_faraday_folder,"figure_scripts","figure_5","Figure_5a_plot_ensemble_profiles.py")
-    figure_5b_path = os.path.join(local_faraday_folder,"figure_scripts","figure_5","Figure_5b_get_helix_sheet_violinplots.py")
-    figure_5c_path = os.path.join(local_faraday_folder,"figure_scripts","figure_5","Figure_5c_apply_average_profiles.py")
-    figure_S2_path = os.path.join(local_faraday_folder,"figure_scripts","supplements","plot_profile_fit.py")
-    figure_S3_path = os.path.join(local_faraday_folder,"figure_scripts","supplements","fsc_dip_analysis_with_rmsd.py")
+    figure_3c_path = os.path.join(local_faraday_folder,"figure_3","Figure_3c_plot_radial_profile_perturbed_modemaps.py")
+    figure_4b_path = os.path.join(local_faraday_folder,"figure_4","Figure_4b_bfactor_distribution_scattered.py")
+    figure_5a_path = os.path.join(local_faraday_folder,"figure_5","Figure_5a_plot_ensemble_profiles.py")
+    figure_5b_path = os.path.join(local_faraday_folder,"figure_5","Figure_5b_get_helix_sheet_violinplots.py")
+    figure_5c_path = os.path.join(local_faraday_folder,"figure_5","Figure_5c_apply_average_profiles.py")
+    figure_S2_path = os.path.join(local_faraday_folder,"supplements","plot_profile_fit.py")
+    figure_S3_path = os.path.join(local_faraday_folder,"supplements","fsc_dip_analysis_with_rmsd.py")
     
     list_of_figure_paths = [figure_2f_path,figure_2g_path,figure_3c_path,figure_4b_path,figure_5a_path,figure_5b_path,figure_5c_path,
                             figure_S2_path,figure_S3_path]
